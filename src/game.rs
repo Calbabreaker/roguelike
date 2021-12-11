61f34205b8b430490b7e83334922679bc3bf70be
Calbabreaker <calbabreaker@gmail.com>
2021-12-12 00:40:03 +1030

    added monster move

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use crate::{
     map::{Map, MAP_HEIGHT, MAP_WIDTH},
-    object::Object,
+    object::{Ai, Fighter, Object},
 };
 use rand::Rng;
 use tcod::console::Console;
@@ -8,8 +8,8 @@ use tcod::console::Console;
 pub const SCREEN_WIDTH: i32 = 80;
 pub const SCREEN_HEIGHT: i32 = 50;
 
-const PLAYER: usize = 0;
-const MAX_ROOM_MONSTERS: i32 = 3;
+const PLAYER_INDEX: usize = 0;
+const MAX_MONSTERS_IN_ROOM: i32 = 5;
 
 pub struct Game {
     root: tcod::console::Root,
@@ -35,7 +35,14 @@ impl Game {
         };
 
         let (start_x, start_y) = game.map.rooms[0].get_center();
-        let player = Object::new(start_x, start_y, '@', tcod::colors::WHITE, "Player", true);
+        let mut player = Object::new(start_x, start_y, '@', tcod::colors::WHITE, "Player", true);
+        player.fighter = Some(Fighter {
+            max_hp: 30,
+            hp: 30,
+            defense: 2,
+            power: 5,
+        });
+
         game.objects.push(player);
 
         for i in 0..game.map.rooms.len() {
@@ -49,13 +56,14 @@ impl Game {
         while !self.root.window_closed() {
             self.render();
             self.handle_input();
+            self.object_update();
         }
     }
 
     fn render(&mut self) {
         self.console.clear();
 
-        let player = &self.objects[PLAYER];
+        let player = &self.objects[PLAYER_INDEX];
         if player.get_pos() != self.prev_player_pos {
             self.map.recaculate_fov(player);
         }
@@ -88,7 +96,6 @@ impl Game {
         use tcod::input::KeyCode::*;
 
         let key = self.root.wait_for_keypress(true);
-
         match key {
             Key {
                 printable: 'f',
@@ -98,21 +105,41 @@ impl Game {
 
             Key { code: Escape, .. } => std::process::exit(0),
 
-            Key { code: Up, .. } => self.move_obj_by(PLAYER, 0, -1),
-            Key { code: Down, .. } => self.move_obj_by(PLAYER, 0, 1),
-            Key { code: Left, .. } => self.move_obj_by(PLAYER, -1, 0),
-            Key { code: Right, .. } => self.move_obj_by(PLAYER, 1, 0),
+            Key { code: Up, .. } => self.move_obj_by(PLAYER_INDEX, 0, -1),
+            Key { code: Down, .. } => self.move_obj_by(PLAYER_INDEX, 0, 1),
+            Key { code: Left, .. } => self.move_obj_by(PLAYER_INDEX, -1, 0),
+            Key { code: Right, .. } => self.move_obj_by(PLAYER_INDEX, 1, 0),
 
-            _ => {
-                let player = &self.objects[PLAYER];
-                if player.alive {
-                    for object in &self.objects {
-                        // make sure object not player
-                        if (object as *const _) != (player as *const _) {
-                            println!("The {} growls!", object.name);
-                        }
-                    }
+            _ => {}
+        }
+    }
+
+    fn object_update(&mut self) {
+        let player = &self.objects[PLAYER_INDEX];
+        if player.alive {
+            for i in 0..self.objects.len() {
+                if self.objects[i].ai.is_some() {
+                    self.ai_basic_turn(i);
                 }
+            }
+        }
+    }
+
+    fn ai_basic_turn(&mut self, monster_index: usize) {
+        let monster = &self.objects[monster_index];
+        let player = &self.objects[PLAYER_INDEX];
+        if self.map.fov_map.is_in_fov(monster.x, monster.y) {
+            // move towards player if too far away
+            if monster.distance(&player) >= 2.0 {
+                // cannot borrow position since function mut borrows self
+                let (player_x, player_y) = player.get_pos();
+                self.move_obj_towards(monster_index, player_x, player_y);
+            } else if player.fighter.as_ref().map_or(false, |f| f.hp > 0) {
+                let monster = &self.objects[monster_index];
+                println!(
+                    "The attack of the {} bounces off your shiny metal armor!",
+                    monster.name
+                );
             }
         }
     }
@@ -128,7 +155,7 @@ impl Game {
     }
 
     fn place_random_objects(&mut self, room_index: usize) {
-        let num_monsters = rand::thread_rng().gen_range(0..MAX_ROOM_MONSTERS + 1);
+        let num_monsters = rand::thread_rng().gen_range(0..MAX_MONSTERS_IN_ROOM + 1);
         let room = &self.map.rooms[room_index];
 
         for _ in 0..num_monsters {
@@ -137,10 +164,12 @@ impl Game {
 
             if !self.is_blocked(x, y) {
                 let monster = match rand::random::<f32>() {
-                    p if p < 0.8 => {
-                        Object::new(x, y, 'o', tcod::colors::DESATURATED_GREEN, "Orc", true)
-                    }
-                    _ => Object::new(x, y, 'T', tcod::colors::DARK_GREEN, "Troll", true),
+                    p if p < 0.8 => Object::new(x, y, 'o', tcod::colors::DARKER_GREEN, "Orc", true)
+                        .add_fighter(10, 10, 0, 3)
+                        .add_ai(Ai::Basic),
+                    _ => Object::new(x, y, 'T', tcod::colors::DARK_GREEN, "Troll", true)
+                        .add_fighter(16, 16, 1, 4)
+                        .add_ai(Ai::Basic),
                 };
 
                 self.objects.push(monster);
@@ -148,29 +177,36 @@ impl Game {
         }
     }
 
-    fn move_obj_by(&mut self, obj_index: usize, dx: i32, dy: i32) {
+    fn move_obj_by(&mut self, obj_index: usize, diff_x: i32, diff_y: i32) {
         let (x, y) = &self.objects[obj_index].get_pos();
-        let (desired_x, desired_y) = (x + dx, y + dy);
+        let (target_x, target_y) = (x + diff_x, y + diff_y);
 
-        if obj_index == PLAYER {
+        if obj_index == PLAYER_INDEX {
             let target_index = self
                 .objects
                 .iter()
-                .position(|object| object.get_pos() == (desired_x, desired_y));
+                .position(|object| object.get_pos() == (target_x, target_y));
 
-            match target_index {
-                Some(target_index) => {
-                    println!(
-                        "The {} laughs at your puny efforts to attack him!",
-                        self.objects[target_index].name
-                    );
-                }
-                None => {}
-            }
+            target_index.map(|target_index| {
+                println!(
+                    "The {} laughs at your puny efforts to attack him!",
+                    self.objects[target_index].name
+                );
+            });
         }
 
-        if !self.is_blocked(desired_x, desired_y) {
-            self.objects[obj_index].set_pos(desired_x, desired_y);
+        if !self.is_blocked(target_x, target_y) {
+            self.objects[obj_index].set_pos(target_x, target_y);
         }
+    }
+
+    fn move_obj_towards(&mut self, obj_index: usize, target_x: i32, target_y: i32) {
+        let (x, y) = &self.objects[obj_index].get_pos();
+        let (diff_x, diff_y) = (target_x - x, target_y - y);
+        let distance = ((diff_x.pow(2) + diff_y.pow(2)) as f32).sqrt();
+
+        let step_x = (diff_x as f32 / distance).round() as i32;
+        let step_y = (diff_y as f32 / distance).round() as i32;
+        self.move_obj_by(obj_index, step_x, step_y);
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,3 +1,17 @@
+// combat related stuff (player, monsters, NPCs)
+#[derive(Debug)]
+pub struct Fighter {
+    pub max_hp: i32,
+    pub hp: i32,
+    pub defense: i32,
+    pub power: i32,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Ai {
+    Basic,
+}
+
 #[derive(Debug)]
 pub struct Object {
     pub x: i32,
@@ -7,6 +21,8 @@ pub struct Object {
     pub name: String,
     pub solid: bool,
     pub alive: bool,
+    pub fighter: Option<Fighter>,
+    pub ai: Option<Ai>,
 }
 
 impl Object {
@@ -19,7 +35,24 @@ impl Object {
             name: name.into(),
             solid,
             alive: true,
+            fighter: None,
+            ai: None,
         };
+    }
+
+    pub fn add_fighter(mut self, max_hp: i32, hp: i32, defense: i32, power: i32) -> Self {
+        self.fighter = Some(Fighter {
+            max_hp,
+            hp,
+            defense,
+            power,
+        });
+        self
+    }
+
+    pub fn add_ai(mut self, ai: Ai) -> Self {
+        self.ai = Some(ai);
+        self
     }
 
     pub fn set_pos(&mut self, x: i32, y: i32) {
@@ -29,6 +62,11 @@ impl Object {
 
     pub fn get_pos(&self) -> (i32, i32) {
         return (self.x, self.y);
+    }
+
+    pub fn distance(&self, other: &Object) -> f32 {
+        let (diff_x, diff_y) = (other.x - self.x, other.y - self.y);
+        ((diff_x.pow(2) + diff_y.pow(2)) as f32).sqrt()
     }
 
     pub fn draw(&self, console: &mut dyn tcod::Console) {
